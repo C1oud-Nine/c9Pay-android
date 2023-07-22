65aa2ba8a352d334a5e52de6aedfef152382396b
kminssk <kminssk@gmail.com>
2023-07-23 07:53:57 +0900

    CL-19 Added activity_pay.xml, activity_result.xml, kt files Updated Kotlin version Changes EN to KO

--- app/src/main/java/com/example/c9pay/ResultActivity.kt
+++ app/src/main/java/com/example/c9pay/ResultActivity.kt
@@ -3,9 +3,9 @@ package com.example.c9pay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_result)
     }
 }
