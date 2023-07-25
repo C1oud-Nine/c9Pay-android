f76056237517197be523ecc3281ce9a61cc3411e
kminssk <kminssk@gmail.com>
2023-07-25 14:06:53 +0900

    CL-47 Added converting Entry <-> Main

--- app/src/main/java/com/example/c9pay/EntryActivity.kt
+++ app/src/main/java/com/example/c9pay/EntryActivity.kt
@@ -1,11 +1,35 @@
 package com.example.c9pay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Button
+
 class EntryActivity : AppCompatActivity() {
+    companion object {
+        var entryActivity : EntryActivity? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry)
+        MainActivity.mainActivity?.finish()
+
+        entryActivity = this
+        val btnSignin = findViewById<Button>(R.id.btnSignin)
+        btnSignin.setOnClickListener {
+            /* 서버 통신 : 아이디 비밀번호 유효한지 확인
+            ~~~~
+            ~~~~
+            */
+
+            // 유효하면, Main 액티비티 실행
+            val toMainIntent = Intent(this@EntryActivity, MainActivity::class.java)
+            startActivity(toMainIntent)
+
+            // 유효하지 않으면, 토스트 메시지
+            // Toast.makeText(this@EntryActivity, "올바르지 않은 ID 및 비밀번호 입니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/c9pay/MainActivity.kt
+++ app/src/main/java/com/example/c9pay/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.c9pay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        var mainActivity : MainActivity? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        EntryActivity.entryActivity?.finish()
+
+        mainActivity = this
+        val btnSignout = findViewById<Button>(R.id.btnSignout)
+        btnSignout.setOnClickListener {
+            val toEntryIntent = Intent(this@MainActivity, EntryActivity::class.java)
+            startActivity(toEntryIntent)
+        }
     }
 }
