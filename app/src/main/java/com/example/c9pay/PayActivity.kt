214f583daa386458fe2ce9abc3026ab732908e5b
kminssk <kminssk@gmail.com>
2023-07-27 17:13:17 +0900

    CL-46--createQR Created function converting string to QR image Printed QR image

--- app/src/main/java/com/example/c9pay/PayActivity.kt
+++ app/src/main/java/com/example/c9pay/PayActivity.kt
@@ -2,15 +2,22 @@ package com.example.c9pay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
+
 
 class PayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
 
+        val imgQR = findViewById<ImageView>(R.id.imgQR)
+
         // 서버 통신 : QR 생성 요청
         // QR 출력
+        val QRstring = "sample string"
+        createQRCode(QRstring, imgQR)
+
 
 
         /*
